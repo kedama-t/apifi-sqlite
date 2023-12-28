be2e5c12d75cec5ca2199bcf8b31ea3ff8f8e09e
Takeda.M <114888622+kedama-t@users.noreply.github.com>
2023-12-28 14:29:18 +0900

    add post/put/delete methods

--- dist/createApp.js
+++ dist/createApp.js
@@ -4,15 +4,74 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const sqlite3_1 = __importDefault(require("sqlite3"));
 const sqlite_1 = require("sqlite");
 const path_1 = __importDefault(require("path"));
-async function createApp(dbPath) {
+const getWhereClauseAndParameters = (query) => {
+    const parameters = [];
+    const whereClause = "WHERE " +
+        Object.keys(query)
+            .map((key) => {
+            const [fieldName, operand] = key.split(",");
+            const value = query[key];
+            const paramCount = value.split(",").length;
+            switch (operand?.toLowerCase()) {
+                case "in":
+                    parameters.push(...value.split(","));
+                    return `${fieldName} in (${Array(paramCount).fill("?").join(",")})`;
+                case "notin":
+                    parameters.push(...value.split(","));
+                    return `${fieldName} not in (${Array(paramCount)
+                        .fill("?")
+                        .join(",")})`;
+                case "like":
+                    parameters.push(value);
+                    return `${fieldName} like '%' || ? || '%'`;
+                case "notlike":
+                    parameters.push(value);
+                    return `not ${fieldName} like '%' || ? || '%'`;
+                case "gt":
+                    parameters.push(value);
+                    return `${fieldName} > ?`;
+                case "lt":
+                    parameters.push(value);
+                    return `${fieldName} < ?`;
+                case "ge":
+                    parameters.push(value);
+                    return `${fieldName} >= ?`;
+                case "le":
+                    parameters.push(value);
+                    return `${fieldName} <= ?`;
+                case "ne":
+                    if (value === "null") {
+                        return `${fieldName} is not null`;
+                    }
+                    parameters.push(value);
+                    return `${fieldName} != ?`;
+                case "eq":
+                case "":
+                default:
+                    if (value === "null") {
+                        return `${fieldName} is null`;
+                    }
+                    parameters.push(value);
+                    return `${fieldName} = ?`;
+            }
+        })
+            .join(" and ");
+    return { whereClause, parameters };
+};
+async function default_1(dbPath) {
     const db = await (0, sqlite_1.open)({
         filename: path_1.default.resolve(dbPath),
         driver: sqlite3_1.default.Database,
     });
     const app = (0, express_1.default)();
+    app.use(body_parser_1.default.urlencoded({
+        extended: true,
+    }));
+    app.use(body_parser_1.default.json());
     // テーブル名を取得し、それぞれのテーブルに対してエンドポイントを設定する
     const tables = await db.all(`SELECT name FROM sqlite_master WHERE type='table'`);
     for (const table of tables) {
@@ -20,58 +79,72 @@ async function createApp(dbPath) {
         // データ取得エンドポイント
         app.get(`/${tableName}`, async (req, res) => {
             const query = req.query;
-            let whereClause = "";
-            if (Object.keys(query).length > 0) {
-                whereClause =
-                    "WHERE " +
-                        Object.keys(query)
-                            .map((key) => {
-                            const [fieldName, operand] = key.split(",");
-                            const value = query[key];
-                            const paramCount = value.split(",").length;
-                            switch (operand?.toLowerCase()) {
-                                case "in":
-                                    return `${fieldName} in (${Array(paramCount)
-                                        .fill("?")
-                                        .join(",")})`;
-                                case "notin":
-                                    return `${fieldName} not in (${Array(paramCount)
-                                        .fill("?")
-                                        .join(",")})`;
-                                case "like":
-                                    return `${fieldName} like '%' || ? || '%'`;
-                                case "notlike":
-                                    return `not ${fieldName} like '%' || ? || '%'`;
-                                case "gt":
-                                    return `${fieldName} > ?`;
-                                case "lt":
-                                    return `${fieldName} < ?`;
-                                case "ge":
-                                    return `${fieldName} >= ?`;
-                                case "le":
-                                    return `${fieldName} <= ?`;
-                                case "ne":
-                                    if (value === "null") {
-                                        return `${fieldName} is not null`;
-                                    }
-                                    return `${fieldName} = ?`;
-                                case "eq":
-                                case "":
-                                default:
-                                    if (value === "null") {
-                                        return `${fieldName} is null`;
-                                    }
-                                    return `${fieldName} = ?`;
-                            }
-                        })
-                            .join(" and ");
-            }
+            const { whereClause, parameters } = Object.keys(query).length > 0
+                ? getWhereClauseAndParameters(query)
+                : { whereClause: "", parameters: [] };
             const stmt = await db.prepare(`SELECT * FROM ${tableName} ${whereClause}`);
-            const values = Object.values(query).reduce((prev, value) => {
-                return [...prev, ...value.split(",")];
-            }, []);
-            const data = await stmt.all(values);
-            res.json(data);
+            try {
+                const data = await stmt.all(parameters);
+                res.status(200).json(data);
+            }
+            catch (e) {
+                res.status(500).json(e);
+            }
+        });
+        // データ削除エンドポイント
+        app.delete(`/${tableName}`, async (req, res) => {
+            const query = req.query;
+            const { whereClause, parameters } = Object.keys(query).length > 0
+                ? getWhereClauseAndParameters(query)
+                : { whereClause: "", parameters: [] };
+            const stmt = await db.prepare(`DELETE FROM ${tableName} ${whereClause}`);
+            try {
+                const result = await stmt.run(parameters);
+                res.status(200).json(result.changes);
+            }
+            catch (e) {
+                res.status(500).json(e);
+            }
+        });
+        // データ作成エンドポイント
+        app.post(`/${tableName}`, async (req, res) => {
+            const body = req.body;
+            const keys = Object.keys(body);
+            const values = Object.values(body);
+            const stmt = await db.prepare(`INSERT INTO ${tableName} (${keys.join(",")}) VALUES (${Array(values.length)
+                .fill("?")
+                .join(",")})`);
+            try {
+                const result = await stmt.run(values);
+                res.status(200).json(result.lastID);
+            }
+            catch (e) {
+                res.status(500).json(e);
+            }
+        });
+        // データ更新エンドポイント
+        app.put(`/${tableName}`, async (req, res) => {
+            const query = req.query;
+            const { whereClause, parameters: whereParameters } = Object.keys(query).length > 0
+                ? getWhereClauseAndParameters(query)
+                : { whereClause: "", parameters: [] };
+            const body = req.body;
+            const setParameters = [];
+            const setClause = Object.keys(body)
+                .map((key) => {
+                setParameters.push(body[key]);
+                return `${key} = ?`;
+            })
+                .join(",");
+            const stmt = await db.prepare(`UPDATE ${tableName} SET ${setClause} ${whereClause}`);
+            console.log({ stmt, setClause, whereClause });
+            try {
+                const result = await stmt.run([...setParameters, ...whereParameters]);
+                res.status(200).json(result.changes);
+            }
+            catch (e) {
+                res.status(500).json(e);
+            }
         });
     }
     const routes = app._router.stack
@@ -93,4 +166,4 @@ async function createApp(dbPath) {
     });
     return { app, routes };
 }
-exports.default = createApp;
+exports.default = default_1;
--- src/createApp.ts
+++ src/createApp.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import type { Request } from "express";
+import bodyParser from "body-parser";
+
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import path from "path";
 
-interface RequestWithQuery extends Request {
+interface IRequest extends Request {
   query: {
-    [key: string]: string;
+    [key: string]: any;
+  };
+  body: {
+    [key: string]: any;
   };
 }
 
@@ -21,7 +26,8 @@ export interface APIRoute {
   path: string;
 }
 
-const getWhereClause = (query: { [key: string]: string }) => {
+const getWhereClauseAndParameters = (query: { [key: string]: string }) => {
+  const parameters: any[] = [];
   const whereClause =
     "WHERE " +
     Object.keys(query)
@@ -32,22 +38,30 @@ const getWhereClause = (query: { [key: string]: string }) => {
 
         switch (operand?.toLowerCase()) {
           case "in":
+            parameters.push(...value.split(","));
             return `${fieldName} in (${Array(paramCount).fill("?").join(",")})`;
           case "notin":
+            parameters.push(...value.split(","));
             return `${fieldName} not in (${Array(paramCount)
               .fill("?")
               .join(",")})`;
           case "like":
+            parameters.push(value);
             return `${fieldName} like '%' || ? || '%'`;
           case "notlike":
+            parameters.push(value);
             return `not ${fieldName} like '%' || ? || '%'`;
           case "gt":
+            parameters.push(value);
             return `${fieldName} > ?`;
           case "lt":
+            parameters.push(value);
             return `${fieldName} < ?`;
           case "ge":
+            parameters.push(value);
             return `${fieldName} >= ?`;
           case "le":
+            parameters.push(value);
             return `${fieldName} <= ?`;
 
           case "ne":
@@ -55,7 +69,8 @@ const getWhereClause = (query: { [key: string]: string }) => {
               return `${fieldName} is not null`;
             }
 
-            return `${fieldName} = ?`;
+            parameters.push(value);
+            return `${fieldName} != ?`;
 
           case "eq":
           case "":
@@ -64,11 +79,12 @@ const getWhereClause = (query: { [key: string]: string }) => {
               return `${fieldName} is null`;
             }
 
+            parameters.push(value);
             return `${fieldName} = ?`;
         }
       })
       .join(" and ");
-      return whereClause;
+  return { whereClause, parameters };
 };
 
 export default async function (dbPath: string) {
@@ -79,6 +95,13 @@ export default async function (dbPath: string) {
 
   const app = express();
 
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(bodyParser.json());
+
   // テーブル名を取得し、それぞれのテーブルに対してエンドポイントを設定する
   const tables: { name: string }[] = await db.all(
     `SELECT name FROM sqlite_master WHERE type='table'`
@@ -88,23 +111,97 @@ export default async function (dbPath: string) {
     const tableName = table.name;
 
     // データ取得エンドポイント
-    app.get(`/${tableName}`, async (req: RequestWithQuery, res) => {
+    app.get(`/${tableName}`, async (req: IRequest, res) => {
       const query = req.query;
-      const whereClause = Object.keys(query).length > 0 ? getWhereClause(query) :"";
+      const { whereClause, parameters } =
+        Object.keys(query).length > 0
+          ? getWhereClauseAndParameters(query)
+          : { whereClause: "", parameters: [] };
 
       const stmt = await db.prepare(
         `SELECT * FROM ${tableName} ${whereClause}`
       );
 
-      const values = Object.values(query).reduce(
-        (prev: string[], value: string) => {
-          return [...prev, ...value.split(",")];
-        },
-        []
+      try {
+        const data = await stmt.all(parameters);
+        res.status(200).json(data);
+      } catch (e: unknown) {
+        res.status(500).json(e);
+      }
+    });
+
+    // データ削除エンドポイント
+    app.delete(`/${tableName}`, async (req: IRequest, res) => {
+      const query = req.query;
+      const { whereClause, parameters } =
+        Object.keys(query).length > 0
+          ? getWhereClauseAndParameters(query)
+          : { whereClause: "", parameters: [] };
+
+      const stmt = await db.prepare(`DELETE FROM ${tableName} ${whereClause}`);
+
+      try {
+        const result = await stmt.run(parameters);
+        res.status(200).json(result.changes);
+      } catch (e: unknown) {
+        res.status(500).json(e);
+      }
+    });
+
+    // データ作成エンドポイント
+    app.post(`/${tableName}`, async (req: IRequest, res) => {
+      const body = req.body;
+
+      const keys = Object.keys(body);
+      const values = Object.values(body);
+
+      const stmt = await db.prepare(
+        `INSERT INTO ${tableName} (${keys.join(",")}) VALUES (${Array(
+          values.length
+        )
+          .fill("?")
+          .join(",")})`
       );
 
-      const data = await stmt.all(values);
-      res.json(data);
+      try {
+        const result = await stmt.run(values);
+        res.status(200).json(result.lastID);
+      } catch (e: unknown) {
+        res.status(500).json(e);
+      }
+    });
+
+    // データ更新エンドポイント
+    app.put(`/${tableName}`, async (req: IRequest, res) => {
+      const query = req.query;
+      const { whereClause, parameters: whereParameters } =
+        Object.keys(query).length > 0
+          ? getWhereClauseAndParameters(query)
+          : { whereClause: "", parameters: [] };
+
+      const body = req.body;
+
+
+      const setParameters: any[] = [];
+
+      const setClause = Object.keys(body)
+        .map((key) => {
+          setParameters.push(body[key]);
+          return `${key} = ?`;
+        })
+        .join(",");
+
+      const stmt = await db.prepare(
+        `UPDATE ${tableName} SET ${setClause} ${whereClause}`
+      );
+      console.log({ stmt,setClause,whereClause });
+
+      try {
+        const result = await stmt.run([...setParameters, ...whereParameters]);
+        res.status(200).json(result.changes);
+      } catch (e: unknown) {
+        res.status(500).json(e);
+      }
     });
   }
 
